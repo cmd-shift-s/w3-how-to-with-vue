4cc73fbcbaa6b148d3cfe534f658e0db5247ca1a
gongzza <gz.us.to@gmail.com>
2017-11-01 13:44:36 +0900

    :tractor:build(layout): add side-menu component
    - h2와 router-link를 이용해서 메뉴들을 표시합니다.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,36 @@
 <template>
   <div id="app">
-    <transition name="page" mode="out-in">
-      <router-view></router-view>
-    </transition>
+
+    <side-menu></side-menu>
+
+    <div class="page">
+      <transition name="page" mode="out-in">
+        <router-view></router-view>
+      </transition>
+    </div>
+
   </div>
 </template>
 
 <script>
+import SideMenu from '@/layout/SideMenu.vue'
+
 export default {
   name: 'app',
+  components: {
+    SideMenu
+  }
 }
 </script>
 
 <style lang="scss">
+html, body {
+  margin: 0;
+  padding: 0;
+}
+.page {
+  margin-left: 220px;
+}
 .page-enter-active, .page-leave-active {
   transition: all .35s;
 }
---
+++ src/layout/SideMenu.vue
@@ -0,0 +1,55 @@
+<template lang="html">
+  <aside class="left-menu">
+    <template v-for="route of routes">
+      <h2>{{route.title}}</h2>
+      <router-link class="link" v-for="component of route.components" :key="component.title" :to="component.path">{{component.name}}</router-link>
+    </template>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: 'side-menu',
+  data() {
+    return {
+      routes: [
+        {
+          title: 'HOW TO',
+          components: [
+            { name: 'HowTo Home', path: '/' }
+          ]
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.left-menu {
+  position: fixed;
+  width: 220px;
+  height: 100%;
+  color: #000;
+  background-color: #f1f1f1;
+}
+h2 {
+  margin: 0;
+  padding: 10px 10px;
+}
+.link {
+  padding: 5px 10px;
+  display: block;
+  color: black;
+  text-decoration: none;
+
+  &:hover {
+    background-color: #cccccc;
+  }
+
+  &.is-active {
+    background-color: #4CAF50;
+    color: white;
+  }
+}
+</style>
