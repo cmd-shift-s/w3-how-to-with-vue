87f44e41aa09fe49110cf9d0740a0ddf489e4515
gongzza <gz.us.to@gmail.com>
2017-11-01 11:33:41 +0900

    :wrench:config(webpack): add alias
    - src 를 가리키는 @ alias 추가

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,8 @@ module.exports = {
   },
   resolve: {
     alias: {
-      'vue$': 'vue/dist/vue.esm.js'
+      'vue$': 'vue/dist/vue.esm.js',
+      '@': path.resolve(__dirname, './src')
     }
   },
   devServer: {
