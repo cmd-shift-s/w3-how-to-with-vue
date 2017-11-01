f64a5c3f43709e1aa181797cd2f28ce139b281d1
gongzza <gz.us.to@gmail.com>
2017-11-01 18:11:08 +0900

    :hammer:refactor(IconBar page): using IconBar component
    - IconBar Example 작성

--- src/pages/menus/IconBar.vue
+++ src/pages/menus/IconBar.vue
@@ -1,12 +1,31 @@
 <template lang="html">
   <div>
-    Icon Bar Page
+    <icon-bar>
+      <a v-for="(menu, index) of menus" @click.prevent="pos = index" :class="{active: pos === index}">
+        <i class="fa" :class="`fa-${menu}`"></i>
+      </a>
+    </icon-bar>
+    <icon-bar is-vertical>
+      <a v-for="(menu, index) of menus" @click.prevent="pos = index" :class="{active: pos === index}">
+        <i class="fa" :class="`fa-${menu}`"></i>
+      </a>
+    </icon-bar>
   </div>
 </template>
 
 <script>
+import IconBar from '@/components/menus/IconBar.vue'
 export default {
-  name: 'icon-bar-page'
+  name: 'icon-bar-page',
+  data() {
+    return {
+      menus: ['home', 'search', 'envelope', 'globe', 'trash'],
+      pos: 0
+    }
+  },
+  components: {
+    IconBar
+  }
 }
 </script>
 
