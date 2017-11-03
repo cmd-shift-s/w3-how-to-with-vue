85d8738a6542f9077014555f4165d9403069d55b
gongzza <gz.us.to@gmail.com>
2017-11-03 10:07:51 +0900

    :hammer:refactor(layout/AppMenu): 구조 개선
    - showMenu 제거
    - Element의 classList 이용

--- src/layout/AppMenu.vue
+++ src/layout/AppMenu.vue
@@ -1,19 +1,19 @@
 <template lang="html">
   <div class="menu">
     <div class="top-menu">
-      <router-link class="menu-item home-button" to="/" exact>
+      <router-link class="menu-button home-button" to="/" exact>
         <i class="fa fa-home"></i>
       </router-link>
-      <a class="menu-item toggle-button" @click="toggleMenu()">
+      <a class="menu-button toggle-button" @click="toggleMenu()">
         <i class="fa fa-bars"></i>
       </a>
 
-      <div class="toggle-menu" :class="{'active': showMenu}">
-        <template v-for="route of routes">
+      <div ref="topMenuItems" class="toggle-menu">
+        <div class="menu-item" v-for="route of routes">
           <h2>{{route.title}}</h2>
           <router-link class="link" v-for="children of route.children" :key="children.path" :to="{name: children.name}" exact>{{children.name}}</router-link>
           <router-link class="link" v-if="!route.children" :to="route.path" exact>{{route.name}}</router-link>
-        </template>
+        </div>
       </div>
     </div>
     <aside class="left-menu">
@@ -22,11 +22,11 @@
           <i class="fa fa-github" aria-hidden="true"></i>
         </a>
       </div>
-      <template v-for="route of routes">
+      <div class="menu-item" v-for="route of routes">
         <h2>{{route.title}}</h2>
         <router-link class="link" v-for="children of route.children" :key="children.path" :to="{name: children.name}" exact>{{children.name}}</router-link>
         <router-link class="link" v-if="!route.children" :to="route.path" exact>{{route.name}}</router-link>
-      </template>
+      </div>
     </aside>
   </div>
 </template>
@@ -38,26 +38,31 @@ export default {
   name: 'app-menu',
   data() {
     return {
-      routes,
-      showMenu: false
+      routes
+    }
+  },
+  computed: {
+    '$topMenuItems'() {
+      return this.$refs.topMenuItems
     }
   },
   watch: {
     '$route'() {
-      if (this.showMenu) {
-        this.showMenu = false
-      }
+      this.closeMenu()
     }
   },
   methods: {
     toggleMenu() {
-      this.showMenu = !this.showMenu
+      this.$topMenuItems.classList.toggle('active')
+    },
+    closeMenu() {
+      this.$topMenuItems.classList.remove('active')
     }
   }
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .top-menu {
   display: none;
   position: fixed;
@@ -65,10 +70,9 @@ export default {
   background-color: #4CAF50;
   width: 100%;
   height: 50px;
-  transition: height 0.5 ease;
   z-index: 999;
 
-  .menu-item {
+  .menu-button {
     padding: 5px 10px;
     font-size: 36px;
     color: #555;
@@ -114,23 +118,27 @@ export default {
     }
   }
 }
-h2 {
-  margin: 0;
-  padding: 10px 10px;
-}
-.link {
-  padding: 5px 10px;
-  display: block;
-  color: black;
-  text-decoration: none;
 
-  &:hover {
-    background-color: #cccccc;
+.menu-item {
+  h2 {
+    margin: 0;
+    padding: 10px 10px;
   }
 
-  &.is-active {
-    background-color: #4CAF50;
-    color: white;
+  .link {
+    padding: 5px 10px;
+    display: block;
+    color: black;
+    text-decoration: none;
+
+    &:hover {
+      background-color: #cccccc;
+    }
+
+    &.is-active {
+      background-color: #4CAF50;
+      color: white;
+    }
   }
 }
 
