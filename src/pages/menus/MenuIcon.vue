6bfb80bc6e57a56a6603562fb665a88d22a53781
gongzza <gz.us.to@gmail.com>
2017-11-14 22:13:23 +0900

    :pencil:text(page/MenuIcon): write header and examples

--- src/pages/menus/MenuIcon.vue
+++ src/pages/menus/MenuIcon.vue
@@ -1,11 +1,13 @@
 <template lang="html">
   <page
     title="Menu Icon"
-    intro="a menu icon with CSS"
-    :examples="examples">
+    intro="a menu icon with CSS">
 
+    <template slot="header">
     <h2>How To Create a Menu Icon</h2>
     <p>If you are not using an icon library, you can create a basic menu icon with CSS:</p>
+    </template>
+
     <sample class="menu-icon-page">
       <div>
         <p>Menu Icon:</p>
@@ -26,6 +28,19 @@
     <h3 class="explain">Example Explained</h3>
     <p>The <code>width</code> and the <code>height</code> property specifies the width and height of each bar.</p>
     <p>We have added a black <code>background-color</code>, and the top and bottom <code>margin</code> is used to create some distance between each bar.</p>
+
+    <hr>
+    <h2 class="title">Animated Icon</h2>
+    <p>Use CSS and JavaScript to change the menu icon to a "cancel/remove" icon when it is clicked on:</p>
+    <h5 class="step">Step 1) Add HTML:</h5>
+    <example-code :code="examples[2]"></example-code>
+    <h5 class="step">Step 2) Add CSS:</h5>
+    <example-code :code="examples[3]"></example-code>
+    <h5 class="step">Step 3) Add JavaScript:</h5>
+    <example-code :code="examples[4]"></example-code>
+    <h3 class="explain">Example Explained</h3>
+    <p>HTML &amp; CSS: We use the same styles as before, only this time, we wrap a container element around each &lt;div&gt; element and we specify a class name for each.</p>
+    <p>The container element is used to show a pointer symbol when the user moves the mouse over the divs (bars). When it is clicked on, it will execute a JavaScript function that adds a new class name to it, which will change the styles of each horizontal bar: the first and the last bar is transformed and rotated to the letter "x". The bar in the middle fades out and becomes invisible.</p>
     </template>
   </page>
 </template>
@@ -50,68 +65,49 @@ export default {
           '  margin: 6px 0;',
           '}',
           '```'].join('\n'),
-        {
-          title: 'Animated Icon',
-          subtitle: 'Use CSS and JavaScript to change the menu icon to a "cancel/remove" icon when it is clicked on:',
-          step: 'Step 1) Add HTML:',
-          name: 'Example',
-          code: ['``` html',
-            '<div class="container" onclick="myFunction(this)">',
-            '  <div class="bar1"></div>',
-            '  <div class="bar2"></div>',
-            '  <div class="bar3"></div>',
-            '</div>',
-            '```'].join('\n')
-        },
-        {
-          step: 'Step 2) Add CSS:',
-          name: 'Example',
-          code: ['``` css',
-            '.container {',
-            '  display: inline-block;',
-            '  cursor: pointer;',
-            '}',
-            '',
-            '.bar1, .bar2, .bar3 {',
-            '  width: 35px;',
-            '  height: 5px;',
-            '  background-color: #333;',
-            '  margin: 6px 0;',
-            '  transition: 0.4s;',
-            '}',
-            '',
-            '/* Rotate first bar */',
-            '.change .bar1 {',
-            '  -webkit-transform: rotate(-45deg) translate(-9px, 6px) ;',
-            '  transform: rotate(-45deg) translate(-9px, 6px) ;',
-            '}',
-            '',
-            '/* Fade out the second bar */',
-            '.change .bar2 {',
-            '  opacity: 0;',
-            '}',
-            '',
-            '/* Rotate last bar */',
-            '.change .bar3 {',
-            '  -webkit-transform: rotate(45deg) translate(-8px, -8px) ;',
-            '  transform: rotate(45deg) translate(-8px, -8px) ;',
-            '}',
-            '```'].join('\n')
-        },
-        {
-          step: 'Step 3) Add JavaScript:',
-          name: 'Example',
-          code: ['``` js',
-            'function myFunction(x) {',
-            '  x.classList.toggle("change");',
-            '}',
-            '```'].join('\n'),
-          explained: [
-            'HTML & CSS: We use the same styles as before, only this time, we wrap a container element around each <div> element and we specify a class name for each.',
-            'The container element is used to show a pointer symbol when the user moves the mouse over the divs (bars). When it is clicked on, it will execute a JavaScript function that adds a new class name to it, which will change the styles of each horizontal bar: the first and the last bar is transformed and rotated to the letter "x". The bar in the middle fades out and becomes invisible.'
-          ].join('\n\n')
-
-        }
+        ['``` html',
+          '<div class="container" onclick="myFunction(this)">',
+          '  <div class="bar1"></div>',
+          '  <div class="bar2"></div>',
+          '  <div class="bar3"></div>',
+          '</div>',
+          '```'].join('\n'),
+        ['``` css',
+          '.container {',
+          '  display: inline-block;',
+          '  cursor: pointer;',
+          '}',
+          '',
+          '.bar1, .bar2, .bar3 {',
+          '  width: 35px;',
+          '  height: 5px;',
+          '  background-color: #333;',
+          '  margin: 6px 0;',
+          '  transition: 0.4s;',
+          '}',
+          '',
+          '/* Rotate first bar */',
+          '.change .bar1 {',
+          '  -webkit-transform: rotate(-45deg) translate(-9px, 6px) ;',
+          '  transform: rotate(-45deg) translate(-9px, 6px) ;',
+          '}',
+          '',
+          '/* Fade out the second bar */',
+          '.change .bar2 {',
+          '  opacity: 0;',
+          '}',
+          '',
+          '/* Rotate last bar */',
+          '.change .bar3 {',
+          '  -webkit-transform: rotate(45deg) translate(-8px, -8px) ;',
+          '  transform: rotate(45deg) translate(-8px, -8px) ;',
+          '}',
+          '```'].join('\n'),
+        ['``` js',
+          'function myFunction(x) {',
+          '  x.classList.toggle("change");',
+          '}',
+          '```'].join('\n')
       ]
     }
   },
