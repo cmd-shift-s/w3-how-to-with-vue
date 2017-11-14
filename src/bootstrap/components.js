c39b555ad215c4a53c280be76397b7043b3ac8ad
gongzza <gz.us.to@gmail.com>
2017-11-14 23:03:34 +0900

    :newspaper:new(layout): add Step
    description:
    - Step을 타입별로 쉽게 보여주기 위해서 추가
    - 코드 중복성을 제거하고자 컴포넌트로 만듬
    
    props:
    - no(required): Step number
    - html, css, js: 추가 될 타입 선언
    - title: 위의 타입중 없을 경우 직접 입력

--- src/bootstrap/components.js
+++ src/bootstrap/components.js
@@ -3,7 +3,9 @@ import Vue from 'vue'
 import Page from '../layout/Page.vue'
 import Sample from '../layout/Sample.vue'
 import ExampleCode from '../layout/ExampleCode.vue'
+import Step from '../layout/Step.vue'
 
 Vue.component('page', Page)
 Vue.component('sample', Sample)
 Vue.component('example-code', ExampleCode)
+Vue.component('step', Step)
---
+++ src/layout/Step.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+  <div class="step">
+    <h5>
+      Step {{no}}) {{type}}:
+    </h5>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'step',
+  props: {
+    no: {
+      type: String,
+      required: true
+    },
+    html: Boolean,
+    css: Boolean,
+    js: Boolean,
+    title: String
+  },
+  computed: {
+    type() {
+      switch (true) {
+        case this.html: return 'Add HTML'
+        case this.css: return 'Add CSS'
+        case this.js: return 'Add JavaScript'
+        default: return this.title
+      }
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
