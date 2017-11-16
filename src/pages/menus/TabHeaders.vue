00a5db28342afaea03f3c7382ca105b9174a8e5f
gongzza <gz.us.to@gmail.com>
2017-11-16 19:39:28 +0900

    :pencil:text(page/TabHeaders): write examples

--- src/pages/menus/TabHeaders.vue
+++ src/pages/menus/TabHeaders.vue
@@ -3,6 +3,12 @@
     title="Tab Headers"
     intro="tabs with CSS and JavaScript"
     class="tab-headers">
+
+    <template slot="header">
+    <h2>Tab Headers</h2>
+    <p>Click on the "city" buttons to display the appropriate header:</p>
+    </template>
+
     <sample>
       <tabs
         position="bottom"
@@ -26,6 +32,20 @@
         </div>
       </tabs>
     </sample>
+
+    <template slot="examples">
+    <hr>
+    <h2 class="title">Create Togglable Tab Headers</h2>
+    <step no="1" html></step>
+    <example-code :code="examples[0]"></example-code>
+    <p>Create buttons to open specific tab content. All &lt;div&gt; elements with <code>class="tabcontent"</code> are hidden by default (with CSS &amp; JS). When the user clicks on a button - it will open the tab content that "matches" this button.</p>
+    <hr>
+    <step no="2" css></step>
+    <p>Style the buttons and the tab content:</p>
+    <example-code :code="examples[1]"></example-code>
+    <step no="3" js></step>
+    <example-code :code="examples[2]"></example-code>
+    </template>
   </page>
 </template>
 
@@ -34,6 +54,96 @@ import Tabs from '@/components/menus/Tabs.vue'
 
 export default {
   name: 'tabs-page',
+  data() {
+    return {
+      examples: [
+        ['``` html',
+          '<div id="London" class="tabcontent">',
+          '  <h3>London</h3>',
+          '  <p>London is the capital city of England.</p>',
+          '</div>',
+          '',
+          '<div id="Paris" class="tabcontent">',
+          '  <h3>Paris</h3>',
+          '  <p>Paris is the capital of France.</p> ',
+          '</div>',
+          '',
+          '<div id="Tokyo" class="tabcontent">',
+          '  <h3>Tokyo</h3>',
+          '  <p>Tokyo is the capital of Japan.</p>',
+          '</div>',
+          '',
+          '<div id="Oslo" class="tabcontent">',
+          '  <h3>Oslo</h3>',
+          '  <p>Oslo is the capital of Norway.</p>',
+          '</div>',
+          '',
+          '<button class="tablink" onclick="openCity(\'London\', this, \'red\')" id="defaultOpen">London</button>',
+          '<button class="tablink" onclick="openCity(\'Paris\', this, \'green\')">Paris</button>',
+          '<button class="tablink" onclick="openCity(\'Tokyo\', this, \'blue\')">Tokyo</button>',
+          '<button class="tablink" onclick="openCity(\'Oslo\', this, \'orange\')">Oslo</button>',
+          '```'].join('\n'),
+        ['``` css',
+          '/* Style the tab buttons */',
+          '.tablink {',
+          '  background-color: #555;',
+          '  color: white;',
+          '  float: left;',
+          '  border: none;',
+          '  outline: none;',
+          '  cursor: pointer;',
+          '  padding: 14px 16px;',
+          '  font-size: 17px;',
+          '  width: 25%;',
+          '}',
+          '',
+          '/* Change background color of buttons on hover */',
+          '.tablink:hover {',
+          '  background-color: #777;',
+          '}',
+          '',
+          '/* Set default styles for tab content */',
+          '.tabcontent {',
+          '  color: white;',
+          '  display: none;',
+          '  padding: 50px;',
+          '  text-align: center;',
+          '}',
+          '',
+          '/* Style each tab content individually */ ',
+          '#London {background-color:red;}',
+          '#Paris {background-color:green;}',
+          '#Tokyo {background-color:blue;}',
+          '#Oslo {background-color:orange;}',
+          '```'].join('\n'),
+        ['``` js',
+          'function openCity(cityName, elmnt, color) {',
+          '  // Hide all elements with class="tabcontent" by default */',
+          '  var i, tabcontent, tablinks;',
+          '  tabcontent = document.getElementsByClassName("tabcontent");',
+          '  for (i = 0; i < tabcontent.length; i++) {',
+          '    tabcontent[i].style.display = "none";',
+          '  }',
+          '',
+          '  // Remove the background color of all tablinks/buttons',
+          '  tablinks = document.getElementsByClassName("tablink");',
+          '  for (i = 0; i < tablinks.length; i++) {',
+          '    tablinks[i].style.backgroundColor = "";',
+          '  }',
+          '',
+          '  // Show the specific tab content',
+          '  document.getElementById(cityName).style.display = "block";',
+          '',
+          '  // Add the specific color to the button used to open the tab content',
+          '  elmnt.style.backgroundColor = color;',
+          '}',
+          '',
+          '// Get the element with id="defaultOpen" and click on it',
+          'document.getElementById("defaultOpen").click();',
+          '```'].join('\n')
+      ]
+    }
+  },
   components: {
     Tabs
   }
