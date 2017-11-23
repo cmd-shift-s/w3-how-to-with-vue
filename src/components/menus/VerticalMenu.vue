06846bde5c582f9546bac609995c576ede71557f
gongzza <gz.us.to@gmail.com>
2017-11-23 11:23:38 +0900

    :newspaper::umbrella: new(component/VerticalMenu)
    description:
    - a 버튼들을 새로로 나열한다.

---
+++ src/components/menus/VerticalMenu.vue
@@ -0,0 +1,35 @@
+<template lang="html">
+  <div class="vertical-menu">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vertical-menu'
+}
+</script>
+
+<style lang="scss">
+.vertical-menu {
+  // width: 200px; /* Set a width if you like */
+  width: 100%;
+
+  a {
+    background-color: #eee; /* Grey background color */
+    color: black; /* Black text color */
+    display: block; /* Make the links appear below each other */
+    padding: 12px; /* Add some padding */
+    text-decoration: none; /* Remove underline from links */
+
+    &:hover {
+      background-color: #ccc; /* Dark grey background on mouse-over */
+    }
+
+    &.active {
+      background-color: #4CAF50; /* Add a green color to the "active/current" link */
+      color: white;
+    }
+  }
+}
+</style>
---
+++ test/unit/components/menus/VerticalMenu.test.js
@@ -0,0 +1,15 @@
+import {mount, renderer} from '#/unit/utils'
+import VerticalMenu from '@/components/menus/VerticalMenu.vue'
+
+describe('VerticalMenu.vue', () => {
+  it('render default', () => {
+    const wrap = mount(VerticalMenu)
+
+    expect(wrap.hasClass('vertical-menu')).toBe(true)
+
+    renderer.renderToString(wrap.vm, (err, str) => {
+      if (err) console.error(err)
+      expect(str).toMatchSnapshot()
+    })
+  })
+})
