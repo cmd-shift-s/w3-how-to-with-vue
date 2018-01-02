445985b130fc9a0b97f74d91461fba9c38e26d30
gongzza <gz.us.to@gmail.com>
2018-01-02 17:30:01 +0900

    :pencil:update examples(page/TopNavigation)

--- src/pages/menus/TopNavigation.vue
+++ src/pages/menus/TopNavigation.vue
@@ -7,6 +7,15 @@
         <a :class="{'is-active': isActive(link)}" v-for="link of links" @click="toggle(link)" v-text="link"></a>
       </top-nav>
     </sample>
+
+    <template slot="examples">
+    <hr>
+    <h2 class="title">Create A Top Navigation Bar</h2>
+    <step no="1" html></step>
+    <example-code :code="examples[0]"></example-code>
+    <step no="2" css></step>
+    <example-code :code="examples[1]"></example-code>
+    </template>
   </page>
 </template>
 
@@ -18,7 +27,50 @@ export default {
   data() {
     return {
       currentLink: '',
-      links: ['Home', 'News', 'Contact', 'About']
+      links: ['Home', 'News', 'Contact', 'About'],
+      examples: [
+        [
+          '``` html',
+          '<div class="topnav" id="myTopnav">',
+          '  <a class="active" href="#home">Home</a>',
+          '  <a href="#news">News</a>',
+          '  <a href="#contact">Contact</a>',
+          '  <a href="#about">About</a>',
+          '</div>',
+          '```'
+        ],
+        [
+          '``` css',
+          '/* Add a black background color to the top navigation */',
+          '.topnav {',
+          '  background-color: #333;',
+          '  overflow: hidden;',
+          '}',
+          '',
+          '/* Style the links inside the navigation bar */',
+          '.topnav a {',
+          '  float: left;',
+          '  color: #f2f2f2;',
+          '  text-align: center;',
+          '  padding: 14px 16px;',
+          '  text-decoration: none;',
+          '  font-size: 17px;',
+          '}',
+          '',
+          '/* Change the color of links on hover */',
+          '.topnav a:hover {',
+          '  background-color: #ddd;',
+          '  color: black;',
+          '}',
+          '',
+          '/* Add a color to the active/current link */',
+          '.topnav a.active {',
+          '  background-color: #4CAF50;',
+          '  color: white;',
+          '}',
+          '```'
+        ]
+      ]
     }
   },
   mounted() {
